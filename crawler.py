aa6b5123340a1100963de940f6a23cb1cc402b12
aditya m sharma <adityambha1999@gmail.com>
2024-04-19 17:03:22 +0530

    wrapped crawler in class

--- crawler.py
+++ crawler.py
@@ -3,37 +3,44 @@ import requests
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize as wt
-links=['https://www.geeksforgeeks.org/']
-url_regex = re.compile(r'https?://\S+')
-
-def remove_stopwords(text):
-    stopWords = set(stopwords.words('english'))
-    words = wt(text)
-    filtered_words = [word for word in words if word.lower() not in stopWords]
-    return ' '.join(filtered_words)
-
-content = {}
-def crawler():
-    for i in links:
-        response = requests.get(i)
-        if response.status_code == 200:
-            soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
-            title = soup.title.string if soup.title else "No Title Found"
-            textinfo = remove_stopwords(soup.get_text())
-            content['title']={'link':i,'info':textinfo}
-            alll = [link.get('href') for link in soup.find_all('a')]
-            for link in alll:
-                if link is None:
-                    continue
-                else:
-                    if url_regex.match(link) and \
-                        not re.search(r'privacy(?:-|_)?(?:policy|notice)|terms(?:-|_)?of(?:-|_)?service', link, re.IGNORECASE)\
-                            and not re.search(r'\.(exe|pdf|zip|rar|tar|gz|dmg|iso)$', link, re.IGNORECASE):
-                        links.append(link)
-                        # print(link)
-            print(content)
-        else:
-            continue
 
 
-crawler()
+class crawler:
+    def __init__(self, lpassed):
+
+        self.links = [lpassed]
+
+
+    def remove_stopwords(self,text):
+        stopWords = set(stopwords.words('english'))
+        words = wt(text)
+        filtered_words = [word for word in words if word.lower() not in stopWords]
+        return ' '.join(filtered_words)
+
+
+    def crawler(self):
+        url_regex = re.compile(r'https?://\S+')
+        content = {}
+        for i in self.links:
+            response = requests.get(i)
+            if response.status_code == 200:
+                soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
+                title = soup.title.string if soup.title else "No Title Found"
+                textinfo = self.remove_stopwords(soup.get_text())
+                content['title']={'link':i,'info':textinfo}
+                all_page_links = [link.get('href') for link in soup.find_all('a')]
+                for link in all_page_links:
+                    if link is None:
+                        continue
+                    else:
+                        if url_regex.match(link) and \
+                            not re.search(r'privacy(?:-|_)?(?:policy|notice)|terms(?:-|_)?of(?:-|_)?service', link, re.IGNORECASE)\
+                                and not re.search(r'\.(exe|pdf|zip|rar|tar|gz|dmg|iso)$', link, re.IGNORECASE):
+                            self.links.append(link)
+                            # print(link)
+                yield content
+            else:
+                continue
+
+
+
