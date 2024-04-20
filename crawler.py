16512b8f8ad7f2fa718872e2a8cad437a3937af7
aditya m sharma <adityambha1999@gmail.com>
2024-04-20 15:27:32 +0530

    added async method

--- crawler.py
+++ crawler.py
@@ -3,44 +3,46 @@ import requests
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize as wt
-
+import asyncio
 
 class crawler:
     def __init__(self, lpassed):
 
         self.links = [lpassed]
 
-
-    def remove_stopwords(self,text):
+    def remove_stopwords(self, text):
         stopWords = set(stopwords.words('english'))
         words = wt(text)
         filtered_words = [word for word in words if word.lower() not in stopWords]
         return ' '.join(filtered_words)
 
-
-    def crawler(self):
+    async def crawler(self):
         url_regex = re.compile(r'https?://\S+')
         content = {}
+        flag = 0
         for i in self.links:
+            flag+=1
             response = requests.get(i)
             if response.status_code == 200:
                 soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
                 title = soup.title.string if soup.title else "No Title Found"
                 textinfo = self.remove_stopwords(soup.get_text())
-                content['title']={'link':i,'info':textinfo}
+                content['title'] = {'link': i, 'info': textinfo}
                 all_page_links = [link.get('href') for link in soup.find_all('a')]
                 for link in all_page_links:
                     if link is None:
                         continue
                     else:
                         if url_regex.match(link) and \
-                            not re.search(r'privacy(?:-|_)?(?:policy|notice)|terms(?:-|_)?of(?:-|_)?service', link, re.IGNORECASE)\
+                                not re.search(r'privacy(?:-|_)?(?:policy|notice)|terms(?:-|_)?of(?:-|_)?service', link,
+                                              re.IGNORECASE) \
                                 and not re.search(r'\.(exe|pdf|zip|rar|tar|gz|dmg|iso)$', link, re.IGNORECASE):
                             self.links.append(link)
                             # print(link)
-                yield content
+                if flag ==10:
+                    yield content
+                    await asyncio.sleep(5)
+                    flag =0
+                else: continue
             else:
                 continue
-
-
-
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#import threading
+import asyncio
+from crawler import crawler
+async def get_data_from_crawler():
+    my_crawler = crawler('https://www.geeksforgeeks.org/')
+    async for data in my_crawler.crawler():
+        print(data)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    asyncio.run(get_data_from_crawler())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
